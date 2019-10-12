1ab1689eb0a364851861fedccd1ce2d7bd53a8dd
Nicolas STURMEL <nicolas@sturmel.com>
2019-10-12 17:20:52 +0200

    better

---
+++ cloud-copy.sh
@@ -0,0 +1,7 @@
+cp -r ../artel-quarra-switch node_modules/media-network-manager-cloud/
+cp -r ../cisco-switch node_modules/media-network-manager-cloud/
+cp -r ../mdns-browser node_modules/media-network-manager-cloud/
+cp -r ../mission-control node_modules/media-network-manager-cloud/
+cp -r ../mnms-client-ws-interface node_modules/media-network-manager-cloud/
+cp -r ../multicast-dns node_modules/media-network-manager-cloud/
+cp -r ../rtsp-sdp-query node_modules/media-network-manager-cloud/
