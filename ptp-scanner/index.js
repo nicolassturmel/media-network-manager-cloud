5ba1f9d682bd8dc578f4022b60a8d351a7481797
Nicolas STURMEL <nicolas@sturmel.com>
2019-10-30 20:57:26 +0100

    Contuing PTP scanner

--- ptp-scanner/index.js
+++ ptp-scanner/index.js
@@ -1,14 +1,58 @@
+"use strict";
+exports.__esModule = true;
 var dgram = require("dgram");
-var process = require("process");
+var MessageType;
+(function (MessageType) {
+    MessageType[MessageType["SYNC"] = 0] = "SYNC";
+    MessageType[MessageType["DELAY_REQ"] = 1] = "DELAY_REQ";
+    MessageType[MessageType["PDELAY_REQ"] = 2] = "PDELAY_REQ";
+    MessageType[MessageType["PDELAY_RESP"] = 3] = "PDELAY_RESP";
+    MessageType[MessageType["FOLLOW_UP"] = 8] = "FOLLOW_UP";
+    MessageType[MessageType["DELAY_RESP"] = 9] = "DELAY_RESP";
+    MessageType[MessageType["PDELAY_RESP_FOLLOW_UP"] = 10] = "PDELAY_RESP_FOLLOW_UP";
+    MessageType[MessageType["ANNOUNCE"] = 11] = "ANNOUNCE";
+    MessageType[MessageType["SIGNALING"] = 12] = "SIGNALING";
+    MessageType[MessageType["MANAGEMENT"] = 13] = "MANAGEMENT";
+    /* marker only */
+    MessageType[MessageType["PTP_MAX_MESSAGE"] = 14] = "PTP_MAX_MESSAGE";
+})(MessageType || (MessageType = {}));
+;
+var PtPPacketHeader = /** @class */ (function () {
+    function PtPPacketHeader(data) {
+        this._data = data;
+    }
+    PtPPacketHeader.prototype.version = function () {
+        return 0x0F & this._data.readInt8(1);
+    };
+    PtPPacketHeader.prototype.messageType = function () {
+        return (0x0F & this._data.readInt8(0));
+    };
+    PtPPacketHeader.prototype.domain = function () {
+        return this._data.readInt8(4);
+    };
+    return PtPPacketHeader;
+}());
 var socket = dgram.createSocket({ type: "udp4", reuseAddr: true });
-var PORT = 320;
+var socket2 = dgram.createSocket({ type: "udp4", reuseAddr: true });
+var PORT = 319;
 var MULTICAST_ADDR = "224.0.1.129";
 socket.bind(PORT);
 socket.on("listening", function () {
     socket.addMembership(MULTICAST_ADDR);
     var address = socket.address();
-    console.log("UDP socket listening on " + address.address + ":" + address.port + " pid: " + process.pid);
     socket.on("message", function (message, rinfo) {
-        console.info("Message from: " + rinfo.address + ":" + rinfo.port + " - " + message);
+        var pack = new PtPPacketHeader(message);
+    });
+});
+socket2.bind(PORT + 1);
+socket2.on("listening", function () {
+    socket2.addMembership(MULTICAST_ADDR);
+    var address = socket2.address();
+    socket2.on("message", function (message, rinfo) {
+        var pack = new PtPPacketHeader(message);
+        if (pack.messageType() == MessageType.ANNOUNCE) {
+            console.log("Annouce !!!!!!");
+            console.info("Message from: " + rinfo.address + ":" + rinfo.port + ", domain " + pack.domain() + " for version " + pack.version());
+        }
     });
 });
--- ptp-scanner/index.ts
+++ ptp-scanner/index.ts
@@ -1,8 +1,43 @@
-const dgram = require("dgram");
-const process = require("process");
+import dgram = require ("dgram");
+import process = require  ("process")
+
+enum MessageType{
+	SYNC=0x0,
+	DELAY_REQ,
+	PDELAY_REQ,
+	PDELAY_RESP,
+	FOLLOW_UP=0x8,
+	DELAY_RESP,
+	PDELAY_RESP_FOLLOW_UP,
+	ANNOUNCE,
+	SIGNALING,
+	MANAGEMENT,
+	/* marker only */
+	PTP_MAX_MESSAGE
+};
+
+class PtPPacketHeader {
+    _data: any;
+    constructor(data) {
+        this._data = data;
+    }
+
+    version() : number {
+        return 0x0F & this._data.readInt8(1)
+    }
+
+    messageType() : number {
+        return (0x0F & this._data.readInt8(0))
+    }
+
+    domain() : number {
+        return this._data.readInt8(4)
+    }
+}
 
 const socket = dgram.createSocket({ type: "udp4", reuseAddr: true });
-const PORT = 320;
+const socket2 = dgram.createSocket({ type: "udp4", reuseAddr: true });
+const PORT = 319;
 const MULTICAST_ADDR = "224.0.1.129";
 
 socket.bind(PORT);
@@ -10,13 +45,21 @@ socket.bind(PORT);
 socket.on("listening", function() {
     socket.addMembership(MULTICAST_ADDR);
     const address = socket.address();
-    console.log(
-      `UDP socket listening on ${address.address}:${address.port} pid: ${
-        process.pid
-      }`
-    );
-
     socket.on("message", function(message, rinfo) {
-        console.info(`Message from: ${rinfo.address}:${rinfo.port} - ${message}`);
+        let pack = new PtPPacketHeader(message)
       });
   });
+  
+  socket2.bind(PORT+1);
+
+  socket2.on("listening", function() {
+      socket2.addMembership(MULTICAST_ADDR);
+      const address = socket2.address();
+      socket2.on("message", function(message, rinfo) {
+          let pack = new PtPPacketHeader(message)
+          if(pack.messageType() == MessageType.ANNOUNCE) {
+              console.log("Annouce !!!!!!")
+              console.info(`Message from: ${rinfo.address}:${rinfo.port}, domain ${pack.domain()} for version ${pack.version()}`);
+            }
+        });
+    });
