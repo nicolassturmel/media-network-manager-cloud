0a1b50fe40529631d4fdd8657df8a0d3fdc17da4
Nicolas STURMEL <nicolas@sturmel.com>
2020-05-15 22:36:14 +0200

    Adding first code of the NMOS data miner

---
+++ nmos-controller/index.ts
@@ -0,0 +1,71 @@
+const request = require('request')
+
+const SwitchPollTime = 5
+
+const commandLineArgs = require('command-line-args')
+
+import { MnMs_node, boolString, MnMs_node_port } from "../types/types"
+
+// Command line arguments
+const optionDefinitions = [
+    { name: 'ip', alias: 'i', type: String, defaultValue: 'localhost:3080' },
+    { name: 'port', alias: 'p', type: String, defaultValue: 'vrnetlab' },
+    { name: 'key', alias: 'k', type: String, defaultValue: 'nokey' },
+    { name: 'id', alias: 'y', type: String, defaultValue: undefined },
+    { name: "missioncontrol", alias: "m", type: String}
+  ]
+
+const options = commandLineArgs(optionDefinitions)
+console.log(options)
+
+var client = require('../mnms-client-ws-interface')
+
+client.challenge(options.key)
+client.setCallback((data) => {console.log(data)})
+client.run(options.missioncontrol)
+client.info({
+    Info: "Artel switch client",
+    ServiceClass: "Switches",
+    id: options.id
+})
+
+// Connecting to switch
+
+var SwitchData = {
+    oldT: 0
+};
+
+var OldValue: object = {}
+var Switch : MnMs_node = { 
+    Name: "Artel",
+    Type: "switch", 
+    IP: options.ip,
+    Schema: 1, 
+    Ports: [], 
+    Multicast: "off", 
+    Neighbour: "",
+    Mac: "", 
+    id: options.id
+};    
+var ActionCount = 0;
+var ClearTime = 0;
+var CountTime = 0;
+var NewData
+
+var postReq = async (path,handle) => {
+    request(path, function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+});
+}
+
+var process = (data) => {
+    console.log(data)
+}
+
+var run = () => {
+    postReq("http://192.168.1.162:3211/x-nmos/query/v1.2/senders/",process)
+}
+
+run()
