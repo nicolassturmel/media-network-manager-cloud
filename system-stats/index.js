12da41d40b74757c70fc3c504fee3b21d536b02c
Nicolas Sturmel <nicolas@sturmel.com>
2020-05-18 22:03:59 +0200

    Ready to push data

--- system-stats/index.js
+++ system-stats/index.js
@@ -10,6 +10,7 @@ var optionDefinitions = [
     { name: 'password', alias: 'p', type: String, defaultValue: 'VR-netlab9' },
     { name: 'key', alias: 'k', type: String, defaultValue: 'nokey' },
     { name: 'id', alias: 'y', type: String, defaultValue: undefined },
+    { name: 'disk', alias: 'd', type: String, defaultValue: "/" },
     { name: "missioncontrol", alias: "m", type: String }
 ];
 var options = commandLineArgs(optionDefinitions);
@@ -33,24 +34,61 @@ var Node = {
     Multicast: "off",
     Neighbour: "",
     Mac: "",
-    id: options.id
+    OtherIPs: [],
+    Macs: [],
+    id: options.id,
+    System: {
+        CPU5s: 0,
+        CPU1min: 0,
+        CPU5min: 0,
+        MemBusy: 0,
+        DiskBuzy: 0
+    }
 };
 var osu = require('node-os-utils');
 var os = require('os');
 var nodeDiskInfo = require('node-disk-info');
+var busyCpu = function (t) {
+    osu.cpu.usage(t * 1000).then(function (d) {
+        if (t == 60) {
+            Node.System.CPU1min = d;
+            try {
+                var disks = nodeDiskInfo.getDiskInfoSync();
+                diskSize(disks);
+            }
+            catch (e) {
+                console.error(e);
+            }
+            console.log(Node);
+        }
+        if (t == 5) {
+            Node.System.CPU5s = d;
+            osu.mem.info().then(function (d) { return Node.System.MemBusy = 100 - d.freeMemPercentage; });
+        }
+        if (t == 300) {
+            Node.System.CPU5min = d;
+            netInts();
+        }
+        busyCpu(t);
+    });
+};
 osu.cpu.usage().then(function (d) { return console.log(d); });
-osu.mem.info().then(function (d) { return console.log(100 - d.freeMemPercentage); });
-os.hostname();
-os.networkInterfaces();
-try {
-    var disks = nodeDiskInfo.getDiskInfoSync();
-    printResults('SYNC WAY', disks);
-}
-catch (e) {
-    console.error(e);
-}
-function printResults(title, disks) {
-    console.log("============ " + title + " ==============\n");
+Node.Name = os.hostname();
+var netInts = function () {
+    var ints = os.networkInterfaces();
+    Object.keys(ints).forEach(function (value, index, tab) {
+        for (var _i = 0, _a = ints[value]; _i < _a.length; _i++) {
+            var int = _a[_i];
+            if (int.family == 'IPv4' && int.internal == false) {
+                Node.IP = int.address;
+                Node.OtherIPs.push(int.address);
+                Node.Mac = int.mac;
+                Node.Macs.push(int.mac);
+            }
+        }
+    });
+};
+function diskSize(disks) {
     for (var _i = 0, disks_1 = disks; _i < disks_1.length; _i++) {
         var disk = disks_1[_i];
         console.log('Filesystem:', disk.filesystem);
@@ -59,5 +97,13 @@ function printResults(title, disks) {
         console.log('Available:', disk.available);
         console.log('Capacity:', disk.capacity);
         console.log('Mounted:', disk.mounted, '\n');
+        console.log(options.disk);
+        if (disk.mounted == options.disk) {
+            Node.System.DiskBuzy = Number(disk.capacity.split("%")[0]);
+        }
     }
 }
+busyCpu(5);
+busyCpu(60);
+busyCpu(300);
+netInts();
--- system-stats/index.ts
+++ system-stats/index.ts
@@ -13,6 +13,7 @@ const optionDefinitions = [
     { name: 'password', alias: 'p', type: String, defaultValue: 'VR-netlab9' },
     { name: 'key', alias: 'k', type: String, defaultValue: 'nokey' },
     { name: 'id', alias: 'y', type: String, defaultValue: undefined },
+    { name: 'disk', alias: 'd', type: String, defaultValue: "/" },
     { name: "missioncontrol", alias: "m", type: String}
   ]
 
@@ -41,36 +42,80 @@ var Node : MnMs_node = {
     Multicast: "off", 
     Neighbour: "",
     Mac: "", 
-    id: options.id
+    OtherIPs: [],
+    Macs: [],
+    id: options.id,
+    System: {
+      CPU5s: 0,
+      CPU1min: 0,
+      CPU5min: 0,
+      MemBusy: 0,
+      DiskBuzy: 0
+    }
 };    
 
 var osu = require('node-os-utils')
 var os = require('os')
 const nodeDiskInfo = require('node-disk-info');
 
+var busyCpu = (t) => {
+  osu.cpu.usage(t*1000).then(d => {
+    if(t == 60) {
+      Node.System.CPU1min = d
+      try {
+        const disks = nodeDiskInfo.getDiskInfoSync();
+        diskSize(disks);
+      } catch (e) {
+        console.error(e);
+      }
+      console.log(Node)
+    }
+    if(t == 5) {
+      Node.System.CPU5s = d
+      osu.mem.info().then(d => Node.System.MemBusy = 100-d.freeMemPercentage)
+    }
+    if(t == 300) {
+      Node.System.CPU5min = d
+      netInts()
+    }
+    busyCpu(t)
+  })
+}
 osu.cpu.usage().then(d => console.log(d))
-osu.mem.info().then(d => console.log(100-d.freeMemPercentage))
-os.hostname()
-os.networkInterfaces()
+Node.Name = os.hostname()
 
-try {
-  const disks = nodeDiskInfo.getDiskInfoSync();
-  printResults('SYNC WAY', disks);
-} catch (e) {
-  console.error(e);
+var netInts = () => {
+  let ints = os.networkInterfaces()
+  Object.keys(ints).forEach((value,index,tab) => {
+    for(let int of ints[value]) {
+      if(int.family == 'IPv4' && int.internal == false)
+      {
+        Node.IP = int.address
+        Node.OtherIPs.push(int.address)
+        Node.Mac = int.mac
+        Node.Macs.push(int.mac)
+      }
+    }
+  })
 }
 
-function printResults(title, disks) {
- 
-  console.log(`============ ${title} ==============\n`);
-
-  for (const disk of disks) {
+function diskSize(disks) {
+ for (const disk of disks) {
       console.log('Filesystem:', disk.filesystem);
       console.log('Blocks:', disk.blocks);
       console.log('Used:', disk.used);
       console.log('Available:', disk.available);
       console.log('Capacity:', disk.capacity);
       console.log('Mounted:', disk.mounted, '\n');
-  }
+      console.log(options.disk)
 
+    if(disk.mounted == options.disk) {
+      Node.System.DiskBuzy = Number(disk.capacity.split("%")[0])
+    }
+  }
 }
+
+busyCpu(5)
+busyCpu(60)
+busyCpu(300)
+netInts()
